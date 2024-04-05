91ca669e91029d4f02f73ca4f77301e1fdd99ffb
Shannon Webster <shanoonwebbie@gmail.com>
2024-04-05 15:03:51 -0700

    refactored code and added in comments.

--- engineering_course_work/labs/calculator-2/calculator.py
+++ engineering_course_work/labs/calculator-2/calculator.py
@@ -4,48 +4,53 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod)
 
 
-# Replace this with your code
-while True:
-    user_input = input("What math operation would you like to perform? Input operator followed by numbers.")
-    user_equation = user_input.split(' ')
-    operator = user_equation[0]
+# create dictionary to asbstract different equations
+operations_dict ={"+":add,
+                  "-":subtract,
+                  "*":multiply,
+                  "/":divide,
+                  "square":square,
+                  "cube":cube,
+                  "pow":power,
+                  "mod":mod
+                  }
 
+def compute_equation(operator, num1, num2=None):
+    if operator in operations_dict:
+        # if a second number is not provided run the operation with one argument
+        if num2 is None:
+            return operations_dict[operator](num1)
+        else:
+            return operations_dict[operator](num1, num2)
+    # if an operator and num(s) are not provided - print an error message
+    else:
+        raise ValueError("Invalid operator. Please try again.")
+
+
+while True:
+    # ask for user to type in a math operation
+    user_input = input("What math operation would you like to perform? Input operator followed by numbers.")
+    # extract the input numbers that are separated with a space
+    user_equation = user_input.split(' ')
+    # assign the math operator to be the first item input
+    operator = user_equation[0]
+    # stop the loop if user inputs q or quit
     if user_input.lower() == "q" or user_input.lower() == "quit":
         print("Goodbye!")
         break
-    # elif len(user_equation) < 2:
-        # print("Error. Insufficient input.")
-    # def compute_equation(operator,selected_equation):
-    #     if operator == selected_equation(num1,num2)
-    # result = operations[operator](num1,num2)
-    # dict ={'+':add, '-':subtract}
         
-    elif len(user_equation) <= 3:
+    elif len(user_equation) >= 2:
         try:
             num1 = float(user_equation[1])
-            num2 = float(user_equation[-1])
-            if operator == "+":
-                result = add(num1,num2)
-            if operator == "-":
-                result = subtract(num1,num2)
-            if operator == "*":
-                result = multiply(num1,num2)
-            if operator == "/":
-                result = divide(num1,num2)
-            if operator == "square":
-                result = square(num1)
-            if operator == "cube":
-                result = cube(num1)
-            if operator == "pow":
-                result = power(num1, num2)
-            if operator == "mod":
-                result = mod(num1, num2)
-                
-            
-            
+            if len(user_equation) == 3:
+                num2 = float(user_equation[-1])
+                result = compute_equation(operator, num1, num2)
+            else:
+                result = compute_equation(operator, num1)
             print(result)            
-        except:
+        except ValueError:
             print("There was an error with your input.")
+        except ZeroDivisionError:
+            ("You can't divide by zero.")
     else:
-        print("Too many numbers.")
-    
+        print("Invalid input format. Please input operator followed by two numbers.")
