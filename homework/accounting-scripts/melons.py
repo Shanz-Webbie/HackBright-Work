fedbd34137b4a6b178018f66e14310dea6552656
Shannon Webster <shanoonwebbie@gmail.com>
2024-04-14 16:12:51 -0700

    dictionaries homework

---
+++ homework/accounting-scripts/melons.py
@@ -0,0 +1,48 @@
+melon_names = {
+    1: 'Honeydew',
+    2: 'Crenshaw',
+    3: 'Crane',
+    4: 'Casaba',
+    5: 'Cantaloupe',
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+flesh_color = { 1: 'red',
+    2: 'orange',
+    3: 'yellow',
+    4: 'green',
+    5: 'red'
+
+}
+
+rind_color = { 1: 'pink',
+    2: 'orange',
+    3: 'yellow',
+    4: 'pink',
+    5: 'red'
+
+}
+
+average_weight = {1: '1 pound',
+    2: '3 pounds',
+    3: '3.6 pounds',
+    4: '2 pounds',
+    5: '2.7 pounds'
+
+}
+
